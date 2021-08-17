67386528fe1f4aebe052a2972e23812b699aef08
GLinBoy <hojjatabedie@gmail.com>
2021-08-17 23:14:35 +0430

    🐛 FIX: Added @RequestBody to controller methods

--- edge/src/main/kotlin/com/glinboy/tapsell/edge/controller/EventController.kt
+++ edge/src/main/kotlin/com/glinboy/tapsell/edge/controller/EventController.kt
@@ -4,20 +4,22 @@ import com.glinboy.tapsell.dto.ClickEvent
 import com.glinboy.tapsell.dto.ImpressionEvent
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/event")
 class EventController {
 
     @PostMapping("/impression")
-    fun impressionEventHandler(event: ImpressionEvent): ResponseEntity<Void> {
+    fun impressionEventHandler(@Valid @RequestBody event: ImpressionEvent): ResponseEntity<Void> {
         return ResponseEntity.ok().build()
     }
 
     @PostMapping("/click")
-    fun clickEventHandler(event: ClickEvent): ResponseEntity<Void> {
+    fun clickEventHandler(@Valid @RequestBody event: ClickEvent): ResponseEntity<Void> {
         return ResponseEntity.ok().build()
     }
 }
