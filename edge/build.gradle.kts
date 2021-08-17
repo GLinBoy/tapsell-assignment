f62a145a4c9be7a4c226d2bcf8c511d99b9bba30
GLinBoy <hojjatabedie@gmail.com>
2021-08-17 19:45:56 +0430

    📦 NEW: Added OpenAPI (Swagger)

--- edge/build.gradle.kts
+++ edge/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.kafka:spring-kafka")
+	implementation("org.springdoc:springdoc-openapi-ui:1.5.10")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.kafka:spring-kafka-test")
 }
