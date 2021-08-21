0eef30b11a840931b419d6686cc2e6503fa561ae
GLinBoy <hojjatabedie@gmail.com>
2021-08-21 15:28:26 +0430

    👌 IMPROVE: Added ImpressionEventModel for save in MongoDB

---
+++ consumer/src/main/kotlin/com/glinboy/tapsell/consumer/entity/ImpressionEventModel.kt
@@ -0,0 +1,17 @@
+package com.glinboy.tapsell.consumer.entity
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document
+data class ImpressionEventModel(
+    @Id
+    val requestId: String = ObjectId.get().toString(),
+    val timestamp: Long,
+    val adId: String,
+    val adTitle: String,
+    val advertiserCost: Double,
+    val appId: String,
+    val appTitle: String
+)
