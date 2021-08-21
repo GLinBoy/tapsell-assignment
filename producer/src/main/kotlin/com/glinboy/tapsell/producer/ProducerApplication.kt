f6548ce39c81d299a338ca8f2e1e4e559c24609a
GLinBoy <hojjatabedie@gmail.com>
2021-08-21 23:06:56 +0430

    👌 IMPROVE: Enabled schedule

--- producer/src/main/kotlin/com/glinboy/tapsell/producer/ProducerApplication.kt
+++ producer/src/main/kotlin/com/glinboy/tapsell/producer/ProducerApplication.kt
@@ -2,8 +2,10 @@ package com.glinboy.tapsell.producer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class ProducerApplication
 
 fun main(args: Array<String>) {
