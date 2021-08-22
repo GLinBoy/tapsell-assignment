f91ad59ca674348182b6ef2926890c395a2c39d0
GLinBoy <hojjatabedie@gmail.com>
2021-08-22 19:42:46 +0430

    📦 NEW: Added OpenAPI (swagger)

--- producer/build.gradle.kts
+++ producer/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("com.github.javafaker:javafaker:1.0.2")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springdoc:springdoc-openapi-ui:1.5.10")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
