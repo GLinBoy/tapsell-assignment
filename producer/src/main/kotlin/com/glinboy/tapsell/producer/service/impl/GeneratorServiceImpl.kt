4580a1d442214820024c78e3968a9d55e252086a
GLinBoy <hojjatabedie@gmail.com>
2021-08-22 14:27:48 +0430

    👌 IMPROVE: create event objects

--- producer/src/main/kotlin/com/glinboy/tapsell/producer/service/impl/GeneratorServiceImpl.kt
+++ producer/src/main/kotlin/com/glinboy/tapsell/producer/service/impl/GeneratorServiceImpl.kt
@@ -1,13 +1,18 @@
 package com.glinboy.tapsell.producer.service.impl
 
+import com.github.javafaker.Faker
+import com.glinboy.tapsell.dto.ClickEvent
+import com.glinboy.tapsell.dto.ImpressionEvent
 import com.glinboy.tapsell.producer.service.GeneratorServiceApi
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import java.time.Instant
 import java.time.LocalDateTime
+import java.util.*
 
 @Service
-class GeneratorServiceImpl:GeneratorServiceApi {
+class GeneratorServiceImpl : GeneratorServiceApi {
 
     private var switch: Boolean = false
     private var probability: Int = 75
@@ -16,10 +21,23 @@ class GeneratorServiceImpl:GeneratorServiceApi {
 
     @Scheduled(fixedRate = 1000)
     override fun generateEvents() {
-        if(switch) {
-            logger.info("Run at: {}", LocalDateTime.now().toString())
+        if (switch) {
+            val impressionEvent = ImpressionEvent(
+                requestId = "0",
+                timestamp = Instant.now().epochSecond,
+                adId = UUID.randomUUID().toString(),
+                adTitle = Faker.instance().funnyName().name(),
+                advertiserCost = 0.0,
+                appId = UUID.randomUUID().toString(),
+                appTitle = Faker.instance().name().title()
+            )
+            logger.info("Impression Event: {}", impressionEvent)
             if ((1..101).random() <= probability) {
-                logger.info("Clicked!")
+                val clickEvent = ClickEvent(
+                    requestId = impressionEvent.requestId,
+                    timestamp = Instant.now().epochSecond
+                )
+                logger.info("Click Event: {}", clickEvent)
             }
         }
     }
