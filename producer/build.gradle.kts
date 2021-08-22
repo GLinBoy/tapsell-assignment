1e9d6c2038b9bdea93b3293d428f57757895f2c6
GLinBoy <hojjatabedie@gmail.com>
2021-08-22 14:27:25 +0430

    📦 NEW: Added Faker dependency

--- producer/build.gradle.kts
+++ producer/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.github.javafaker:javafaker:1.0.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
