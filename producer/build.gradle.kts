561b1a4c05950583a5886a96a144fb5c9916bc82
GLinBoy <hojjatabedie@gmail.com>
2021-08-22 16:53:37 +0430

    🐛 FIX: Jackson deserializer result of API calling

--- producer/build.gradle.kts
+++ producer/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("com.github.javafaker:javafaker:1.0.2")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
